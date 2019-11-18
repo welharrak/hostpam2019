f43196de2b31ff45f754a6195b9908db150c3151
Walid el Harrak <harrak31@gmail.com>
2019-11-18 09:42:10 +0100

    pam contra ldap

---
+++ hostpam19:auth/install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+useradd local1
+useradd local2
+useradd local3
+echo "local1" | passwd --stdin local1
+echo "local2" | passwd --stdin local2
+echo "local3" | passwd --stdin local3
+cp /opt/docker/login.defs /etc/login.defs
+cp /opt/docker/system-auth /etc/pam.d/system-auth
+cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
+cp /opt/docker/nslcd.conf /etc/nslcd.conf
+cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+
