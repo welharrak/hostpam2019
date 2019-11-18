2bf6f98b044627df3a173c5331dc73a0173bb47a
Walid el Harrak <harrak31@gmail.com>
2019-11-18 08:53:18 +0100

    pam:base

--- hostpam19:base/install.sh
+++ hostpam19:base/install.sh
@@ -6,3 +6,5 @@ echo "local1" | passwd --stdin local1
 echo "local2" | passwd --stdin local2
 echo "local3" | passwd --stdin local3
 cp /opt/docker/login.defs /etc/login.defs
+cp /opt/docker/system-auth /etc/pam.d/system-auth
+cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
