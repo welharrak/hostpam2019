aa80338d128b6b3bd08175b0972799d58d5ecd10
Walid el Harrak <harrak31@gmail.com>
2019-11-22 10:43:26 +0100

    pam scratch + authconfig

---
+++ hostpam19:scratch/install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+useradd local1
+useradd local2
+useradd local3
+echo "local1" | passwd --stdin local1
+echo "local2" | passwd --stdin local2
+echo "local3" | passwd --stdin local3
+cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
+cp /opt/docker/nslcd.conf /etc/nslcd.conf
+cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+authconfig --enableshadow --enablelocauthorize --enableldap --enableldapauth --ldapserver='ldapserver' --ldapbase='dc=edt,dc=org' --enablemkhomedir --updateall
---
+++ hostpam19:scratch/startup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+bash /opt/docker/install.sh
+/sbin/nscd
+/sbin/nslcd
