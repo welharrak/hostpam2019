5b214d48cc6ccd39fa285b03d20d53e560b427c8
Walid el Harrak <harrak31@gmail.com>
2019-11-25 09:50:04 +0100

    scratch

--- hostpam19:scratch/install.sh
+++ hostpam19:scratch/install.sh
@@ -8,4 +8,5 @@ echo "local3" | passwd --stdin local3
 cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+#athconfig per poder-nos autenticar com usuaris ldap
 authconfig --enableshadow --enablelocauthorize --enableldap --enableldapauth --ldapserver='ldapserver' --ldapbase='dc=edt,dc=org' --enablemkhomedir --updateall
--- hostpam19:scratch/startup.sh
+++ hostpam19:scratch/startup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 bash /opt/docker/install.sh
 /sbin/nscd
+#para hacerlo detach ponmos -n al final
 /sbin/nslcd
